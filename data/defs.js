870c9344d8e0104980be9367f181c6c1903349b6
beaverpark <pkh7802@gmail.com>
2017-10-15 23:14:36 -0400

    add definitions file

---
+++ data/defs.js
@@ -0,0 +1,7 @@
+module.exports = {
+	'house_type_options': {0: '일반주택', 1:'오피스텔', 2:'레지던스'},
+	'room_type_options': {0: '원룸'},
+	'num_amenities': 18,
+	'pref_currency': {0: 'KRW', 1: 'USD'},
+	'pref_language': {0: 'KOR', 1: 'ENG'}
+};
